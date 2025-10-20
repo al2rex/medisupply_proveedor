993be99ac47eb02960bc8ff2cdc3c12edf7b96e6
Arnaldo Castilla <arcastil@redhat.com>
2025-10-19 19:09:35 -0500

    centraliizando valores parametrriocos

--- medisupply/backend/Program.cs
+++ medisupply/backend/Program.cs
@@ -1,3 +1,7 @@
+using backend.Config;
+using backend.Controllers;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configurar strongly-typed settings
+builder.Services.Configure<PulsarSettings>(
+    builder.Configuration.GetSection("PulsarSettings")
+);
+
+// Registrar como singleton directo
+builder.Services.AddSingleton(sp =>
+    sp.GetRequiredService<IOptions<PulsarSettings>>().Value
+);
+
+
+
+builder.Services.AddSingleton<PulsarService>();
 
 var app = builder.Build();
 
@@ -21,5 +39,16 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+var pulsarService = app.Services.GetRequiredService<PulsarService>();
+await pulsarService.SuscribirseASolicitudProveedorAsync();
+
+app.MapPost("/inventario", async (PulsarService pulsar, string mensaje) =>
+{
+    await pulsar.PublicarInventarioRecibidoAsync(mensaje);
+    return Results.Ok("Mensaje publicado");
+});
+
+
 
 app.Run();
